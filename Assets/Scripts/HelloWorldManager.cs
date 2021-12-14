b382d7736869980dfe74281a18954250d9ef710a
Ewan <qingchen.hou@chukong-inc.com>
2021-12-14 17:02:52 +0800

    + add Bolt plugin * use Bolt to sync variables and RPCs

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -15,8 +15,6 @@ namespace HelloWorld
             else
             {
                 StatusLabels();
-
-                SubmitNewPosition();
             }
 
             GUILayout.EndArea();
@@ -37,16 +35,6 @@ namespace HelloWorld
             GUILayout.Label("Transport: " +
                 NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
             GUILayout.Label("Mode: " + mode);
-        }
-
-        static void SubmitNewPosition()
-        {
-            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
-            {
-                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                var player = playerObject.GetComponent<HelloWorldPlayer>();
-                player.Move();
-            }
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Bolt;
 using HelloWorld;
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour, IMixedRealityInputHandler
+public class PlayerController : NetworkBehaviour, IMixedRealityInputHandler
 {
-    private void OnEnable()
-    {
-        CoreServices.InputSystem?.RegisterHandler<IMixedRealityInputHandler>(this);
-    }
-
-    private void OnDisable()
-    {
-        CoreServices.InputSystem?.UnregisterHandler<IMixedRealityInputHandler>(this);
-    }
-
     private NetworkObject _networkObject;
-    private HelloWorldPlayer _player;
+    private PlayerController _player;
+    
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    public Vector3 replicatedPos = new Vector3();
+    
     private void Start()
     {
         _networkObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
         _networkObject.TryGetComponent(out _player);
+    }
+    void Update()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            Position.Value = transform.position;
+            replicatedPos = Position.Value;
+        }
+        else
+        {
+            replicatedPos = Position.Value;
+        }
+    }
+
+    private void OnEnable()
+    {
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealityInputHandler>(this);
+    }
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            Debug.Log("OnNetworkSpawn local client id:" + NetworkManager.Singleton.LocalClientId);
+        }
     }
 
     public void OnInputUp(InputEventData eventData)
@@ -36,8 +55,30 @@ public class PlayerController : MonoBehaviour, IMixedRealityInputHandler
     {
         if (_player != null && eventData.InputSource.Pointers[0].BaseCursor!= null)
         {
-            _player.PlayerMove(eventData.InputSource.Pointers[0].BaseCursor
+            CustomEvent.Trigger(_player.gameObject,"SetCursorDes", eventData.InputSource.Pointers[0].BaseCursor
                 .Position);
         }
+    }
+    
+    
+    [ServerRpc]
+    private void SubmitCursorPosServerRpc(Vector3 pos)
+    {
+        CustomEvent.Trigger(gameObject, "SetDestination", pos);
+    }
+
+    public void BoltSubmitCursorPosToServer(Vector3 pos)
+    {
+        SubmitCursorPosServerRpc(pos);
+    }
+        
+    public bool IsServer()
+    {
+        return NetworkManager.Singleton.IsServer;
+    }
+    
+    private void OnDisable()
+    {
+        CoreServices.InputSystem?.UnregisterHandler<IMixedRealityInputHandler>(this);
     }
 }
